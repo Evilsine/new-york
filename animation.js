7c4b0e82ea135061ce649f222dfdf91994bc60b1
Evilsine <rejorjiao@gmail.com>
2021-07-05 17:11:45 +0900

    Add files via upload

--- animation.js
+++ animation.js
@@ -13,3 +13,37 @@ observerTexts = new IntersectionObserver(entries => {
 texts.forEach(text =>{
   observerTexts.observe(text)
 })
+
+const showMap = document.querySelectorAll('.show-map');
+const mapPage = document.querySelectorAll('.map-background');
+const mapWindow = document.querySelectorAll('.map-window');
+
+
+showMap.forEach(map => {
+  map.addEventListener('click',() =>{
+    mapPage.forEach(mappage => {
+      mappage.style.display = "block";
+    });
+    mapWindow.forEach(mapwindow => {
+      mapwindow.style.animation = `show-map-animation 1s`;
+    });
+  })
+});
+
+
+mapPage.forEach(mappage => {
+  mappage.addEventListener('click',() =>{
+    mapWindow.forEach(mapwindow => {
+      mapwindow.style.animation = "close-map-animation 0.5s";
+      setTimeout(function(){
+        mapPage.forEach(mappage => {
+            mappage.style.display = "none";
+        });
+        mapwindow.style.animation = "none";
+      }, 500);
+    });
+  })
+
+});
+
+
